c847eac69c15a556f8a7655df3981978b50a4842
pengshengpeng <2409341175@qq.com>
2020-02-09 18:28:31 +0800

    增加hive的配置信息

--- bdpj/common/src/main/java/pdm/bdpj/common/constant/NHttpStatusEnum.java
+++ bdpj/common/src/main/java/pdm/bdpj/common/constant/NHttpStatusEnum.java
@@ -11,13 +11,16 @@ public enum NHttpStatusEnum {
     AUTH_ERROR(403, "000003", "没有权限，拒绝访问"),
     PARAMETER_ERROR(400, "000004", "请求参数类型错误"),
 
+
     /**
      * 文件错误
      */
     EXCEL_TABLE_LIST_EMPTY(500, "100000", "Excel表信息数据为空"),
     EXCEL_DATA_TYPE_MAPPING_EMPTY(500, "100001", "Excel映射关系表为空"),
     EXCEL_SHEET_NOT_EXIST(500, "100002", "Excel表字段信息数据为空"),
-    EXCEL_FILE_NOT_EXIST(500, "100003", "Excel文件不存在");
+    EXCEL_FILE_NOT_EXIST(500, "100003", "Excel文件不存在"),
+
+    SQL_DATA_EMPTY(501, "200001", "sql数据为空");
 
     private final int status;
     private final String code;
--- bdpj/hivetools/src/main/java/pdm/bdpj/hivetools/service/excel/impl/DataAnalysisServiceImpl.java
+++ bdpj/hivetools/src/main/java/pdm/bdpj/hivetools/service/excel/impl/DataAnalysisServiceImpl.java
@@ -1,8 +1,60 @@
 package pdm.bdpj.hivetools.service.excel.impl;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.dao.DataAccessException;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import pdm.bdpj.common.constant.NHttpStatusEnum;
+import pdm.bdpj.common.exception.ServiceException;
+import pdm.bdpj.hivetools.service.excel.DataAnalysisService;
+
+import java.util.List;
+
 /**
  * @Author pengshengpeng
  * @Date 2020/2/9 17:19
  **/
-public class DataAnalysisServiceImpl {
+@Slf4j
+@Service
+public class DataAnalysisServiceImpl implements DataAnalysisService {
+
+    @Autowired
+    private JdbcTemplate hiveDruidTemplate;
+
+    @Transactional(rollbackFor = RuntimeException.class)
+    @Override
+    public boolean createTable(List<String> list) {
+        if (list == null || list.size() <= 0) {
+            throw new ServiceException(NHttpStatusEnum.SQL_DATA_EMPTY);
+        }
+        for (String sql : list) {
+            try {
+                hiveDruidTemplate.execute(sql);
+            } catch (DataAccessException e) {
+                log.error("创建表失败，sql:{}", sql, e.getMessage());
+                return false;
+            }
+        }
+        return true;
+    }
+
+    @Transactional(rollbackFor = RuntimeException.class)
+    @Override
+    public boolean insertIntoTable(List<String> list) {
+        if (list == null || list.size() <= 0) {
+            throw new ServiceException(NHttpStatusEnum.SQL_DATA_EMPTY);
+        }
+        for (String sql : list) {
+            try {
+                hiveDruidTemplate.execute(sql);
+            } catch (DataAccessException e) {
+                log.error("插入表数据失败，sql:{}", sql, e.getMessage());
+                return false;
+            }
+        }
+        return true;
+    }
 }
