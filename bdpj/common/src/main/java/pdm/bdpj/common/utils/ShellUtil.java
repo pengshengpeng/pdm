87a48c498f5634f7d067b49c09f0cfcb360bfa4d
pengshengpeng <2409341175@qq.com>
2020-02-10 19:47:27 +0800

    增加生成shell文件的工具类

--- bdpj/common/src/main/java/pdm/bdpj/common/utils/ShellUtil.java
+++ bdpj/common/src/main/java/pdm/bdpj/common/utils/ShellUtil.java
@@ -9,11 +9,16 @@ import java.io.FileWriter;
  * @Date 2020/2/10 16:07
  **/
 public class ShellUtil {
-
-    //创建shell
-    public static void createExcuteHiveShell(String path,String fileName) throws Exception {
-
-        File sh = new File(path,fileName);
+    /**
+     * 创建执行sql的shell文件
+     *
+     * @param path      生成shell文件的存放目录
+     * @param tableName sql表名
+     * @throws Exception 异常
+     */
+    public static void createExcuteHiveShell(String path, String tableName) throws Exception {
+        String fileName = "exec_" + tableName + "_yyyymmdd.sh";
+        File sh = new File(path, fileName);
         if (sh.exists()) {
             sh.delete();
         }
@@ -21,11 +26,10 @@ public class ShellUtil {
         sh.setExecutable(true);
         FileWriter fw = new FileWriter(sh);
         BufferedWriter bf = new BufferedWriter(fw);
-        new
-        for (int i = 0; i < strs.length; i++) {
-            bf.write(strs[i]);
-
-            if (i < strs.length - 1) {
+        String[] hiveCommond = {"date=$1", "echo ${date}", "hive --hiveconf yyyymmdd='${date}' -f create_" + tableName + "_${date}.sql"};
+        for (int i = 0; i < hiveCommond.length; i++) {
+            bf.write(hiveCommond[i]);
+            if (i < hiveCommond.length - 1) {
                 bf.newLine();
             }
         }
@@ -33,5 +37,37 @@ public class ShellUtil {
         bf.close();
     }
 
+    /**
+     * 创建导入数据的shell文件
+     *
+     * @param path      生成shell文件的存放目录
+     * @param tableName sql表名
+     * @throws Exception 异常
+     */
+    public static void createPutHiveShell(String path, String tableName) throws Exception {
+        String fileName = "put_" + tableName + "_yyyymmdd.sh";
+        File sh = new File(path, fileName);
+        if (sh.exists()) {
+            sh.delete();
+        }
+        sh.createNewFile();
+        sh.setExecutable(true);
+        FileWriter fw = new FileWriter(sh);
+        BufferedWriter bf = new BufferedWriter(fw);
+        String[] hiveCommond = {"originPath=$1", "targetPath=$2", "echo ${originPath}", "echo ${targetPath}", "hdfs dfs -put ${originPath} ${targetPath}"};
+        for (int i = 0; i < hiveCommond.length; i++) {
+            bf.write(hiveCommond[i]);
+            if (i < hiveCommond.length - 1) {
+                bf.newLine();
+            }
+        }
+        bf.flush();
+        bf.close();
+    }
 
+    public static void main(String[] args) throws Exception {
+        File file = new File(NDateUtil.getDays());
+        ShellUtil.createExcuteHiveShell(file.getPath(), "O_LDM_DSR");
+        ShellUtil.createPutHiveShell(file.getPath(), "O_LDM_DSR");
+    }
 }
